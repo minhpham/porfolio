a997b73f10a2bae982aee4a038a7fb2be2c6995d
MinhPham <quachthienminh@gmail.com>
2020-03-09 21:11:11 +0700

    text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello motor</h1>
+        <h1>Hello motorola</h1>
         <p>
           Edit <code>src/App.js</code>
         </p>
