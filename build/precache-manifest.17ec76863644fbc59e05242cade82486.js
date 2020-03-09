b069ceed86e5845032adb4b81e8f0d15065dc08f
MinhPham <quachthienminh@gmail.com>
2020-03-09 16:50:05 +0700

    build

---
+++ build/precache-manifest.17ec76863644fbc59e05242cade82486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1054f2acce98b22ae65ad598618c402",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "481d5b448f1369c3eff7",
+    "url": "/porfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c46ca6fd686dc653637",
+    "url": "/porfolio/static/js/2.2625ec20.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/porfolio/static/js/2.2625ec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481d5b448f1369c3eff7",
+    "url": "/porfolio/static/js/main.ae289cb1.chunk.js"
+  },
+  {
+    "revision": "99dd6d535eee4ec0fd7e",
+    "url": "/porfolio/static/js/runtime-main.6602b64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/porfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
